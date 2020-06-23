ec0917f63cdfaacd091677342cbfaa5cb2e142ae
Tyler Pachal <tylerpachalandfriends@gmail.com>
2020-06-23 18:43:36 -0400

    Add test scenarios

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# elixir-error-handling-blogpost
+# ErrorHandlingBlogpost
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `error_handling_blogpost` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:error_handling_blogpost, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/error_handling_blogpost](https://hexdocs.pm/error_handling_blogpost).
+
---
+++ test/error_handling_blogpost_test.exs
@@ -0,0 +1,145 @@
+defmodule ErrorHandlingBlogpostTest do
+  @moduledoc """
+  Running this test file will result in a lot of errors and failing tests and
+  that is expected.  Most of these tests are about proving that certain
+  combinations of errors and error handling are not compatible.
+
+  This is just a way to organize the handlers (rescue, catch) against the
+  triggers (raise, throw, etc).
+  """
+  use ExUnit.Case
+
+
+  describe "catch/2" do
+    # raise & catch - compatible
+    test "is compatible with raise" do
+      try do
+        raise "raise error"
+      catch
+        :error, value ->
+          %RuntimeError{message: "raise error"} = value
+      end
+    end
+
+    # throw & catch - compatible
+    test "is not compatible with throw" do
+      try do
+        throw "throw error"
+      catch
+        :throw, "throw error" ->
+          :ok
+      end
+    end
+
+    # exit & catch - compatible
+    test "is not compatible with exit" do
+      try do
+        exit("exiting")
+      catch
+        :exit, "exiting" ->
+          :ok
+      end
+    end
+
+    # :erlang.error & catch - compatible
+    test "is compatible with :erlang.error" do
+      try do
+        :erlang.error("erlang error")
+      catch
+        :error, "erlang error" ->
+          :ok
+      end
+    end
+  end
+
+
+  describe "rescue/1" do
+    # raise & rescue - compatible
+    test "is compatible with raise" do
+      try do
+        raise "raise error"
+      rescue
+        e ->
+          %RuntimeError{message: "raise error"} = e
+      end
+    end
+
+    # throw & rescue - not compatible
+    test "is not compatible with throw" do
+      try do
+        throw "throw error"
+      rescue
+        _e ->
+          # Never going to reach here
+          nil
+      end
+    end
+
+    # exit & rescue - not compatible
+    test "is not compatible with exit" do
+      try do
+        exit("exiting")
+      rescue
+        _e ->
+          # Never going to reach here
+          nil
+      end
+    end
+
+    # :erlang.error & rescue - compatible
+    test "is compatible with :erlang.error" do
+      try do
+        :erlang.error("erlang error")
+      rescue
+        e ->
+          %ErlangError{original: "erlang error"} = e
+      end
+    end
+  end
+
+
+  describe "catch/1" do
+    # raise & catch - not compatible
+    test "is compatible with raise" do
+      try do
+        raise "raise error"
+      catch
+        _e ->
+          # Never going to reach here
+          nil
+      end
+    end
+
+    # throw & catch - compatible
+    test "is not compatible with throw" do
+      try do
+        throw "throw error"
+      catch
+        "throw error" ->
+          :ok
+      end
+    end
+
+    # exit & catch - not compatible
+    test "is not compatible with exit" do
+      try do
+        exit("exiting")
+      catch
+        _e ->
+          # Never going to reach here
+          nil
+      end
+    end
+
+    # :erlang.error & catch - not compatible
+    test "is compatible with :erlang.error" do
+      try do
+        :erlang.error("erlang error")
+      catch
+        _e ->
+          # Never going to reach here
+          nil
+      end
+    end
+  end
+end
